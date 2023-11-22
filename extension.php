e3bcc61ba95f5f9e476fb756b98a80ee91cb4b31
Sander Van Damme <73292023+sander-van-damme@users.noreply.github.com>
2023-11-22 22:38:55 +0100

    refactor

--- extension.php
+++ extension.php
@@ -20,17 +20,30 @@ class AutoAssignLabelExtension extends Minz_Extension
 		$entryDao = FreshRSS_Factory::createEntryDao();
 		$tagDao = FreshRSS_Factory::createTagDao();
 
-		# Get all unread entries.
+		// Get all entries.
 		$entries = iterator_to_array($entryDao->selectAll());
+		if (!$entries) {
+			Minz_Log::info("Auto Assign Label Extension: No entries found.");
+		}
+
+		// Get all unread entries.
 		$unreadEntries = array_filter($entries, function ($entry) {
 			if (isset($entry['is_read']) && $entry['is_read'] === true) {
 				return false;
 			}
 			return true;
 		});
+		if (!$unreadEntries) {
+			Minz_Log::info("Auto Assign Label Extension: No unread entries found.");
+		}
 
-		# Add labels to unread entries.
+		// Get labels for unread entries.
 		$unreadEntriesWithLabels = $this->getEntriesWithLabels($unreadEntries);
+		if ($unreadEntriesWithLabels) {
+			Minz_Log::info("Auto Assign Label Extension: No labels created.");
+		}
+
+		// Assign labels to unread entries.
 		foreach ($unreadEntriesWithLabels as $entry) {
 			$entryId = $entry["id"];
 			$this->unassignEntryTags($entryId);
@@ -39,74 +52,96 @@ class AutoAssignLabelExtension extends Minz_Extension
 		}
 	}
 
-
 	private function getTagId($tagName)
 	{
 		$tagDao = FreshRSS_Factory::createTagDao();
-		$tagId = null;
 		// If tag exists.
 		if ($tag = $tagDao->searchByName($tagName)) {
-			$tagId = $tag->id();
+			return $tag->id();
 		}
 		// If tag does not exist.
 		else {
-			$tagId = $tagDao->addTag(['name' => $tagName]);
+			return $tagDao->addTag(['name' => $tagName]);
 		}
-		return $tagId;
 	}
 
 	private function unassignEntryTags($entryId)
 	{
 		$tagDao = FreshRSS_Factory::createTagDao();
 		$tags = $tagDao->getTagsForEntry($entryId);
-		foreach ($tags as $tag) {
-			$tagId = $tag["id"];
-			$tagDao->tagEntry($tagId, $entryId, false);
+		if ($tags) {
+			foreach ($tags as $tag) {
+				$tagDao->tagEntry($tag["id"], $entryId, false);
+			}
 		}
 	}
 
 	private function getEntriesWithLabels($entries)
 	{
-		// Define external API.
-		$apiUrl = FreshRSS_Context::$user_conf->auto_assign_label_api_url;
-		$apiKey = FreshRSS_Context::$user_conf->auto_assign_label_api_key;
+		try {
+			// Assert that entries is not null.
+			if (is_null($entries)) {
+				throw new Exception("Entries must not be null.");
+			}
 
-		// Create request.
-		$curl = curl_init();
-		curl_setopt($curl, CURLOPT_FAILONERROR, true);
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($curl, CURLOPT_TIMEOUT, 300);
+			// Define external API.
+			$apiUrl = FreshRSS_Context::$user_conf->auto_assign_label_api_url;
+			$apiKey = FreshRSS_Context::$user_conf->auto_assign_label_api_key;
 
-		// Set request URL.
-		curl_setopt($curl, CURLOPT_URL, $apiUrl);
+			// Create request.
+			$curl = curl_init();
+			curl_setopt($curl, CURLOPT_FAILONERROR, true);
+			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($curl, CURLOPT_TIMEOUT, 300);
 
-		// Set request method.
-		curl_setopt($curl, CURLOPT_POST, true);
+			// Set request URL.
+			curl_setopt($curl, CURLOPT_URL, $apiUrl);
 
-		// Set request headers.
-		$requestHeaders = array(
-			"Accept: application/json",
-			"Content-Type: application/json",
-			"X-Open-Ai-Api-Key: {$apiKey}"
-		);
-		curl_setopt($curl, CURLOPT_HTTPHEADER, $requestHeaders);
+			// Set request method.
+			curl_setopt($curl, CURLOPT_POST, true);
 
-		// Set request body.
-		$requestBody = array_map(function ($entry) {
-			return [
-				'id' => $entry['id'],
-				'title' => substr($entry['title'], 0, 1000),
-				'content' => substr($entry['content'], 0, 2000),
-			];
-		}, $entries);
-		$requestBodyJson = json_encode($requestBody);
-		curl_setopt($curl, CURLOPT_POSTFIELDS, $requestBodyJson);
+			// Set request headers.
+			$requestHeaders = array(
+				"Accept: application/json",
+				"Content-Type: application/json",
+				"X-Open-Ai-Api-Key: {$apiKey}"
+			);
+			curl_setopt($curl, CURLOPT_HTTPHEADER, $requestHeaders);
 
-		// Get response.
-		$curl_response = curl_exec($curl);
-		$response = json_decode($curl_response);
-		curl_close($curl);
+			// Set request body.
+			$requestBody = array_map(function ($entry) {
+				return [
+					'id' => $entry['id'],
+					'title' => substr($entry['title'], 0, 1000),
+					'content' => substr($entry['content'], 0, 2000),
+				];
+			}, $entries);
+			$requestBodyJson = json_encode($requestBody);
+			curl_setopt($curl, CURLOPT_POSTFIELDS, $requestBodyJson);
 
-		return $response;
+			// Get response.
+			$curl_response = curl_exec($curl);
+
+			// Handle curl errors.
+			if ($curl_response === false) {
+				$error_msg = curl_error($curl);
+				throw new Exception($error_msg);
+			}
+
+			// Decode response.
+			$response = json_decode($curl_response);
+
+			// Assert that decoded response is not null.
+			if (is_null($response)) {
+				throw new Exception("Response must not be null.");
+			}
+
+			return $response;
+		} catch (Exception $e) {
+			Minz_Log::error("Auto Assign Label Extension internal error: " . $e->getMessage());
+			return [];
+		} finally {
+			curl_close($curl);
+		}
 	}
 }
